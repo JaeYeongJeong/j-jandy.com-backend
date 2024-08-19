e954eae90fef8bddcfd4d3f9fa81df5fa31f40ab
JaeYeong Jeong <94372210+JaeYeongJeong@users.noreply.github.com>
2024-08-19 19:01:56 +0900

    Catch Deployment error

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import session from 'express-session';
 import cors from 'cors';
-import fs from 'fs';
-import https from 'https';
+// import fs from 'fs';
+// import https from 'https';
 import noteRoutes from './route/note.js';
 import authRoutes from './route/auth.js';
 import { createSessionConfig } from './config/config.js';
 import mongoDb, { mongodbSessionStore } from './data/mongodb.js';
 
 const PORT = process.env.PORT || 3000;
-
-const options = {
-  key: fs.readFileSync('../localhost-key.pem'),
-  cert: fs.readFileSync('../localhost.pem'),
-};
 
 const app = express();
 
@@ -52,8 +47,12 @@ await mongoDb.connectMongoDb().then(() => {
     console.log(`Server is running on port ${PORT}`);
   });
 
-  https.createServer(options, app).listen(PORT, () => {
-    console.log(`HTTPS server started on port 8080`);
-  });
+  // const options = {
+  //   key: fs.readFileSync('../localhost-key.pem'),
+  //   cert: fs.readFileSync('../localhost.pem'),
+  // };
+  // https.createServer(options, app).listen(PORT, () => {
+  //   console.log(`HTTPS server started on port 8080`);
+  // });
 });
 
