9e2c1c2a0f073028f657050c1473b1a6ede92f6b
JaeYeong Jeong <94372210+JaeYeongJeong@users.noreply.github.com>
2024-08-22 21:15:21 +0900

    Change cookie config

--- config/config.js
+++ config/config.js
@@ -9,9 +9,10 @@ const createSessionConfig = (session_DB) => {
     saveUninitialized: false,
     store: session_DB,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 24시간
-      secure: true, // HTTPS를 사용하는 경우 true로 설정
-      sameSite: 'lax', // 필요에 따라 'strict' 또는 'none'으로 변경
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: true,
+      httpOnly: true,
+      sameSite: 'lax',
     },
   };
 }
