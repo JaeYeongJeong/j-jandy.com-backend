28db4b98326230f151988174c614f9b0ee97c3ef
JaeYeong Jeong <94372210+JaeYeongJeong@users.noreply.github.com>
2024-10-03 20:03:55 +0900

    Change Session error Message

--- middlewares/auth-protection.js
+++ middlewares/auth-protection.js
@@ -1,6 +1,6 @@
 const sessionIsAuthenticated = (req, res, next) => {
   if (!req.session.isAuthenticated) {
-    return res.status(401).json({ message: 'Please log in' });
+    return res.status(401).json({ message: 'Session is anauthenticated' });
   }
 
   next();
